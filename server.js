c6e4853146fb21fd3f4779783c1c1e86d060451c
hyekwang <hyekwang1227@naver.com>
2025-12-12 23:14:37 +0900

    Update server.js

--- server.js
+++ server.js
@@ -295,6 +295,7 @@ app.delete("/api/posts/:id", async (req, res) => {
 // -----------------------------
 // 서버 시작
 // -----------------------------
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
